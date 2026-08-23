340f2b4479d1ec2bcc9db9c07107f5203c660cad
Yves <sevyns.pro@gmail.com>
2026-08-24 00:09:46 +0200

    3 test valides

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 pub fn est_majeur(age: u32) -> bool {
-    if age > 18 {
+    if age >= 18 {
         return true;
     }
     false
@@ -13,5 +13,17 @@ mod tests {
     fn check_age_clairement_majeur() {
         let sevy = est_majeur(24);
         assert!(sevy);
+    }
+
+    #[test]
+    fn check_age_clairement_mineur() {
+        let sevy = est_majeur(9);
+        assert!(!sevy)
+    }
+
+    #[test]
+    fn check_pile_mineur_a_18_ans() {
+        let sevy = est_majeur(18);
+        assert!(sevy)
     }
 }
