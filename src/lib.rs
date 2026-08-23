6830cccaf7865255e90b733276c09585d2f5ccde
Yves <sevyns.pro@gmail.com>
2026-08-24 00:25:03 +0200

    premier test valide

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,27 @@ pub fn est_majeur(age: u32) -> bool {
     false
 }
 
+//Écris une fonction moyenne qui prend une slice de notes (&[f64]) et renvoie leur moyenne.
+pub fn moyenne(notes: &[f64]) -> f64 {
+
+    let mut moyenne: f64 = 0.0;
+    let diviseur = notes.len() as f64;
+
+    for note in notes.iter() {
+        moyenne += note;
+    }
+    moyenne / diviseur
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn check_moyenne_de_10_et_20_est_de_15() {
+        let notes: Vec<f64> = vec![10.0, 20.0];
+        assert_eq!(moyenne(&notes), 15.0);
+    }
 
     #[test]
     fn check_age_clairement_majeur() {
