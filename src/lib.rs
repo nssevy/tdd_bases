5c6b263cf8292a5aa35f1742ba020fdb430e671a
Yves <sevyns.pro@gmail.com>
2026-08-24 00:51:52 +0200

    tout les test passent

--- src/lib.rs
+++ src/lib.rs
@@ -8,13 +8,15 @@ pub fn est_majeur(age: u32) -> bool {
 //Écris une fonction moyenne qui prend une slice de notes (&[f64]) et renvoie leur moyenne.
 pub fn moyenne(notes: &[f64]) -> f64 {
 
-    let mut moyenne: f64 = 0.0;
-    let diviseur = notes.len() as f64;
-
-    for note in notes.iter() {
-        moyenne += note;
+    if notes.is_empty() {
+        panic!("Le tableau est vide");
     }
-    moyenne / diviseur
+
+    let diviseur = notes.len() as f64;
+    let somme: f64 = notes.iter().sum();
+
+    somme / diviseur
+
 }
 
 #[cfg(test)]
@@ -25,6 +27,13 @@ mod tests {
     fn check_moyenne_de_10_et_20_est_de_15() {
         let notes: Vec<f64> = vec![10.0, 20.0];
         assert_eq!(moyenne(&notes), 15.0);
+    }
+
+    #[test]
+    #[should_panic]
+    fn check_que_la_fn_moyenne_panic_si_le_tableau_est_vide(){
+        let notes: Vec<f64> = vec![];
+        moyenne(&notes);
     }
 
     #[test]
@@ -40,7 +49,7 @@ mod tests {
     }
 
     #[test]
-    fn check_pile_mineur_a_18_ans() {
+    fn check_pile_majeur_a_18_ans() {
         let sevy = est_majeur(18);
         assert!(sevy)
     }
